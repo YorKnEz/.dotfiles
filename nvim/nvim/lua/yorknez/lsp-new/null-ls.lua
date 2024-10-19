fdedbd94b2cd99c89fda7c31a4ff1ec4f85dd856
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-10-19 22:40:30 +0300

    change python formatter settings

--- nvim/nvim/lua/yorknez/lsp-new/null-ls.lua
+++ nvim/nvim/lua/yorknez/lsp-new/null-ls.lua
@@ -5,7 +5,9 @@ end
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.black.with({
+      extra_args = { '--line-length', '120' }
+    }),
     null_ls.builtins.formatting.isort,
 
     -- js
