99b4e98ea14ed74b63e2ee4177b1de34142eccaa
Alex Mitreanu <alexmitreanu04@gmail.com>
2025-03-19 15:13:28 +0200

    add vimtex

--- nvim/nvim/lua/yorknez/treesitter.lua
+++ nvim/nvim/lua/yorknez/treesitter.lua
@@ -4,11 +4,11 @@ if not status_ok then
 end
 
 configs.setup({
-  ensure_installed = "all", -- one of "all" or a list of languages
-  ignore_install = { "" }, -- List of parsers to ignore installing
+  ensure_installed = "all",     -- one of "all" or a list of languages
+  ignore_install = { "latex" }, -- List of parsers to ignore installing
   highlight = {
-    enable = true,         -- false will disable the whole extension
-    disable = { "css" },   -- list of language that will be disabled
+    enable = true,              -- false will disable the whole extension
+    disable = { "css" },        -- list of language that will be disabled
   },
   autopairs = {
     enable = true,
